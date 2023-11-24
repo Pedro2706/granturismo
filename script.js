8448394cfbbf5f19f18b551a58045333dc41ca7f
Pedro2706 <150362875+Pedro2706@users.noreply.github.com>
2023-11-24 09:22:03 -0300

    terminei mais esta com alguns erros

--- script.js
+++ script.js
@@ -17,5 +17,49 @@ let positions = [
 function init(){
     selected = [];
 
-    currentPlayer.innerHTML = `JOGADOR DA VEZ: $(player)`
+    currentPlayer.innerHTML = `JOGADOR DA VEZ: $(player)`;
+
+  document.querySelectorAll(".game button").forEach((item) => {
+    item.innerHTML = "";
+    item.addEventListenner("click", newMove);
+  });
+}
+
+init();
+
+function newMove(e){
+    const index = e.target.getAttribute("data-1");
+    e.target.innerHTML = player;
+    e.target.removeEventlistenner("click", newMove);
+    selected[index] = player
+
+    setTimeout(() =>{
+        check();
+      }, [100]);
+
+      player = player === "x" ? "O" : "x";
+      currentPlayer.innerHTML = `JOGADOR DA VEZ: $(player)`;
+    }
+
+    function check(){
+        let playerLastMove = player === "x" ? "O" : "x";
+
+        const itens = selected
+           .map((item, i) => [item, i])
+           .filter((item) => item[0] === playerLastMove)
+           .map((item) =>[1]);
+          
+        for (pos of positions){
+            if (pos.every((item)=> itens.includes(item))) {
+                alert("O JOGADOR '" + playerLastMove + "'GANHOU!");
+                init();
+                return;
+    }
+  }
+
+  if (selected,filter((item) => item).length === 9){
+    alert("DEU EMPATE!");
+    init();
+    return;
+  }
 }
